e524c84513c52f78802ef35c2055a91ae1b61cf2
Tarqiul Islam <adwise.bangladesh@gmail.com>
2024-07-04 20:02:59 +0600

    Update Navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () =>{
     const { cartItems } = useSelector((state:any) => state.cart)
     return(
         <nav className="bg-white border-gray-200 bx-shadow">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
                 <button data-collapse-toggle="navbar-multi-level" type="button" 
                     className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 
                     rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 
